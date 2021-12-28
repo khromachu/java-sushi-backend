8d7292c5fdc7bddc5ea6eb9d5a9760f63ee6ecc2
khromachu <nadushok.khrom777@gmail.com>
2021-12-29 01:50:20 +0700

    init

---
+++ src/main/java/edu/tpu/khromachu/sushibackend/SushiBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.tpu.khromachu.sushibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SushiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tpu/khromachu/sushibackend/SushiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tpu.khromachu.sushibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
